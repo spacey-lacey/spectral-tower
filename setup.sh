05f8649ccbcf3e11f4ad5bd9bbca56ade948c08e
spacey-lacey <jlovelacerainbolt@gmail.com>
2024-12-03 17:40:55 -0600

    renamed file to match documentation

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # set repo directory in mapping interface to pwd
-mapping_interface_file="bit_slicer/mapping_interface_script.py"
+interface_file="bit_slicer/mapping_interface.py"
 
-if sed -i "" "12s|Path(\".*\")|Path(\"$(pwd)\")|" $mapping_interface_file
+if sed -i "" "12s|Path(\".*\")|Path(\"$(pwd)\")|" $interface_file
 then
-    echo "Updated git_repo_path in $mapping_interface_file to $(pwd)"
+    echo "Updated git_repo_path in $interface_file to $(pwd)"
 else
-    echo "Error: Could not update git_repo_path in $mapping_interface_file"
+    echo "Error: Could not update git_repo_path in $interface_file"
 fi
